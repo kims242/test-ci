49364aecc166ea1c948c76036e1c7280dcce0ef7
kims242 <144317483+kims242@users.noreply.github.com>
2026-01-15 16:08:21 +0100

    Update prisma-mock.ts

--- app/src/tests/prisma-mock.ts
+++ app/src/tests/prisma-mock.ts
@@ -2,6 +2,6 @@ import { PrismaClient } from '@prisma/client';
 import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 // On force le type pour éviter les erreurs TS2615
-const prismaMock = mockDeep<PrismaClient>() as unknown as DeepMockProxy<PrismaClient>;
+const prismaMock = mockDeep<PrismaClient>() as any;
 
 export default prismaMock;
