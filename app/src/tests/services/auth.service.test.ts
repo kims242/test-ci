7a8a2bb9f8f9797df4ef526f2bc9a0bb346df2c8
kims242 <144317483+kims242@users.noreply.github.com>
2026-01-15 15:19:52 +0100

    Update auth.service.test.ts

--- app/src/tests/services/auth.service.test.ts
+++ app/src/tests/services/auth.service.test.ts
@@ -1,43 +1,44 @@
 import prismaMock from '../prisma-mock';
-import { registerUser, loginUser } from '../../app/routes/auth/auth.service';
+import { createUser, login } from '../../app/routes/auth/auth.service';
 
 describe('AuthService', () => {
-  test('registerUser should return created user', async () => {
-    const mockedExistingUser = {
+  test('createUser should return created user', async () => {
+    prismaMock.user.findUnique.mockResolvedValue(null);
+
+    prismaMock.user.create.mockResolvedValue({
       id: 1,
       username: 'TestUser',
       email: 'test@me',
-      password: '1234',
       bio: null,
       image: null,
-      token: '',
-      demo: false,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    } as any);
 
-    prismaMock.user.findUnique.mockResolvedValue(null);
-    prismaMock.user.create.mockResolvedValue(mockedExistingUser);
+    const result = await createUser({
+      username: 'TestUser',
+      email: 'test@me',
+      password: '1234',
+    });
 
-    await expect(registerUser('TestUser', 'test@me', '1234')).resolves.toHaveProperty('username', 'TestUser');
+    expect(result).toHaveProperty('username', 'TestUser');
+    expect(result).toHaveProperty('token');
   });
 
-  test('loginUser should return existing user', async () => {
-    const mockedExistingUser = {
+  test('login should return existing user', async () => {
+    prismaMock.user.findUnique.mockResolvedValue({
       id: 1,
       username: 'TestUser',
       email: 'test@me',
-      password: '1234',
+      password: await require('bcryptjs').hash('1234', 10),
       bio: null,
       image: null,
-      token: '',
-      demo: false,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+    } as any);
 
-    prismaMock.user.findUnique.mockResolvedValue(mockedExistingUser);
+    const result = await login({
+      email: 'test@me',
+      password: '1234',
+    });
 
-    await expect(loginUser('test@me', '1234')).resolves.toHaveProperty('email', 'test@me');
+    expect(result).toHaveProperty('email', 'test@me');
+    expect(result).toHaveProperty('token');
   });
 });
