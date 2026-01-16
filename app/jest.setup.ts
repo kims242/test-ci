c77135a4a982774dcd8ebc2d3b883fac0d109079
kims242 <144317483+kims242@users.noreply.github.com>
2026-01-16 01:57:41 +0100

    Update jest.setup.ts

--- app/jest.setup.ts
+++ app/jest.setup.ts
@@ -1,6 +1,7 @@
 import prismaMock from './prisma/prisma-mock';
 
-jest.mock('../../prisma/client', () => ({
-  __esModule: true,
-  default: prismaMock,
-}));
+jest.mock('@prisma/client', () => {
+  return {
+    PrismaClient: jest.fn(() => prismaMock),
+  };
+});
