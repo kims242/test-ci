d976feeef70d26ba262bf32f2c4c995a5e693b40
kims242 <144317483+kims242@users.noreply.github.com>
2026-01-16 02:03:09 +0100

    Update prisma-mock.ts

--- app/prisma/prisma-mock.ts
+++ app/prisma/prisma-mock.ts
@@ -3,3 +3,13 @@ import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 const prismaMock = mockDeep<PrismaClient>() as unknown as DeepMockProxy<PrismaClient>;
 export default prismaMock;
+
+const mockedArticleResponse = {
+  id: 'article-1',
+  title: 'Test Article',
+  content: 'Content',
+  _count: { favoritedBy: 0 }, // <- essentiel
+  favoritedBy: [], // si utilisé
+};
+prismaMock.article.update.mockResolvedValue(mockedArticleResponse);
+
